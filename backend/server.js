9817b0188153befa226dcbfb157c9066c124eac1
rarillka <mariabragina08@gmail.com>
2025-12-09 20:37:03 +0300

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql')
+
+const app = express();
+
+app.listen(8081, ()=>{
+    console.log('Listening')
+})
